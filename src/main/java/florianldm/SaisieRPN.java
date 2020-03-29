0eb272f1f2a17b72724e9c0a3e7fe815c28eb853
= <ledeme.florian@gmail.com>
2020-03-29 19:19:10 +0200

    Ajout de SaisieRPN pour gérer les intéractions utilisateur

--- src/main/java/florianldm/SaisieRPN.java
+++ src/main/java/florianldm/SaisieRPN.java
@@ -1,5 +1,35 @@
 package florianldm;
+import java.util.Scanner;
 
 public class SaisieRPN {
+    /** Scanner. */
+    private Scanner scanner;
+    /** Scanner2. */
+    private Scanner scanner2;
+    /** MoteurRPN. */
+    MoteurRPN M = new MoteurRPN();
 
+    /**
+     * Constructeur.
+     */
+    public SaisieRPN(){
+        this.scanner = new Scanner(System.in);
+        this.scanner2 = new Scanner(System.in);
+    }
+
+    /**
+     * Getter.
+     * @return scanner.
+     */
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    /**
+     * Getter.
+     * @return scanner2.
+     */
+    public Scanner getScanner2() {
+        return scanner2;
+    }
 }
