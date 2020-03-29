6a42ffbc1fbeeebaf297c9024cd94d16278d5024
= <ledeme.florian@gmail.com>
2020-03-29 19:17:24 +0200

    Ajout de MoteurRPN + Interface pour les commandes de MoteurRPN

---
+++ src/main/java/florianldm/IcommandeRPN.java
@@ -0,0 +1,8 @@
+package florianldm;
+
+public interface IcommandeRPN {
+    /**
+     * Va appeler méthode du moteurRPN.
+     */
+    void execute();
+}
--- src/main/java/florianldm/MoteurRPN.java
+++ src/main/java/florianldm/MoteurRPN.java
@@ -1,5 +1,43 @@
 package florianldm;
 
-public class MoteurRPN {
+import java.util.Stack;
 
+public class MoteurRPN {
+    /** Pile contenant les opérandes. */
+    private Stack<Double> operandes;
+
+    /**
+     * Constructeur MoteurRPN.
+     */
+    public MoteurRPN() {
+        this.operandes = new Stack<>();
+    }
+
+    /**
+     * Ajout d'une opérande dans la pile.
+     * @param o opérande.
+     */
+    public void addOperande(final double o) {
+        Double d = o;
+        this.operandes.push(d);
+    }
+
+    /**
+     * Effectue une opération sur les deux opérandes.
+     * @param operation operation à effectuer.
+     * @return résultat dans la pile opérandes.
+     */
+    public double makeOperation(final Operation operation) {
+        Double ope = this.operandes.pop();
+        Double ope2 = this.operandes.pop();
+        return this.operandes.push(operation.eval(ope, ope2));
+    }
+
+    /**
+     * Retourne l'ensemble des opérandes de la pile.
+     * @return .
+     */
+    public Stack<Double> getOperandes() {
+        return this.operandes;
+    }
 }
